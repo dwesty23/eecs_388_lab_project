052b6f490f7627a765e55dca242b8227a7b4ba2c
LandonPyko <pykomar2@gmail.com>
2023-04-25 09:37:13 -0500

    Finished commenting

--- milestone2/prog.py
+++ milestone2/prog.py
@@ -1,14 +1,13 @@
 import serial
 import csv
 
-with open('milestone2.csv') as csv_file:  # Open the csv file. Will close the csv file when done running
+with open('milestone2/milestone2.csv') as csv_file:  # Open the csv file. Will close the csv file when done running
     with serial.Serial('/dev/ttyAMA1' , 115200 , timeout = 1) as ser1:  # Creates a serial chanel over /dev/ttyAMA1 with a baudrate of 115200. Sets it to ser1 so it's easy to call functions on
-        reader = csv.reader(csv_file, delimiter=',')  #  
-        for row in reader:
-            angle = str(row[0])  # 
-            speed = str(row[1])
-            duration = str(row[2])
+        reader = csv.reader(csv_file, delimiter=',')  #  Creates a reader object that holds a list of lists of all of the data
+        for row in reader:  # Iterate through each row in the entire reader file
+            angle = str(row[0])  # Assign the angle to the first item
+            speed = str(row[1])  # Assign the speed to the second item
+            duration = str(row[2])  # Assign the duration to the third item
             command_str = "angle: " + angle + ", speed: " + speed + ", duration: " + duration  # Set the command string to be sent to the Pi
-            print(command_str)
-            # ser1.write(bytes(command_str))  # Write the command string to the Pi in terms of bytes, because write() requires bytes to send info
+            ser1.write(bytes(command_str))  # Write the command string to the Pi in terms of bytes, because write() requires bytes to send info
 
