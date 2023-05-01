21190e3855c855dafb312b9c6e3be44e1d4c95a6
Aiden Patel <aiden.patel21@gmail.com>
2023-05-01 11:12:23 -0500

    updated prog.py

--- milestone2/prog.py
+++ milestone2/prog.py
@@ -1,13 +1,19 @@
 import serial
 import csv
+import time
+
+ser1 = serial.Serial(port = '/dev/ttyAMA1' , baudrate = 115200)
 
 with open('milestone2/milestone2.csv') as csv_file:  # Open the csv file. Will close the csv file when done running
-    with serial.Serial('/dev/ttyAMA1' , 115200 , timeout = 1) as ser1:  # Creates a serial chanel over /dev/ttyAMA1 with a baudrate of 115200. Sets it to ser1 so it's easy to call functions on
-        reader = csv.reader(csv_file, delimiter=',')  #  Creates a reader object that holds a list of lists of all of the data
-        for row in reader:  # Iterate through each row in the entire reader file
-            angle = str(row[0])  # Assign the angle to the first item
-            speed = str(row[1])  # Assign the speed to the second item
-            duration = str(row[2])  # Assign the duration to the third item
-            command_str = "angle: " + angle + ", speed: " + speed + ", duration: " + duration  # Set the command string to be sent to the Pi
-            ser1.write(bytes(command_str))  # Write the command string to the Pi in terms of bytes, because write() requires bytes to send info
+    #with serial.Serial('/dev/ttyAMA1' , 115200 , timeout = 1) as ser1:  # Creates a serial chanel over /dev/ttyAMA1 with a baudrate of 115200. Sets it to ser1 so it's easy to call functions on
+    reader = csv.reader(csv_file, delimiter=',')  #  Creates a reader object that holds a list of lists of all of the data
+    for row in reader:  # Iterate through each row in the entire reader file
+        angle = str(row[0])  # Assign the angle to the first item
+        speed = str(row[1])  # Assign the speed to the second item
+        duration = str(row[2])  # Assign the duration to the third item
+        command_str = "angle: " + angle + ", speed: " + speed + ", duration: " + duration  # Set the command string to be sent to the Pi
+        ser1.write(command_str.encode())  # Write the command string to the Pi in terms of bytes, because write() requires bytes to send info
+        time.sleep(int(row[2]) + 1) # time.sleep(5)
+
+ser1.close()
 
